827d529398f10764f6f798e3abba4673e501ea24
ElenaZateeva <zateevaelena14@gmail.com>
2022-11-13 15:15:06 +0300

    Проверка сторон треугольника. Возможность существовния треугольника с заданными сторонами

--- Exam040/Program.cs
+++ Exam040/Program.cs
@@ -1,57 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 40 
+// Программа на входе получает 3 числа и проверяет: могут ли данные значения 
+// являться сторонами треугольника
 
-string[] ReverseArray(string[] array)
+Console.WriteLine("Введите 3 числа:");
+int a1 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+int c1 = Convert.ToInt32(Console.ReadLine());
+bool temp = ItsTriangle(a1, b1, c1);
+Console.WriteLine($"{temp}");
+
+bool ItsTriangle(int a, int b, int c)
 {
-    string[] reversed = new string[array.Length];
-
-    for (var i = 0; i < array.Length; i++)
-    {
-        reversed[i] = array[array.Length - 1 - i];
-    }
-
-    return reversed;
+    return (a + b > c & b + c > a & a + c > b);
 }
-
-///////
-string result = ConvertDecToBinary(4); //вызвали метод
-Console.WriteLine(result); //вывели на экран
-string ConvertDecToBinary(int number) //объявили метод
-{
-    string tmp = "";
-    while (number > 0)
-    {
-        int remainder = number % 2;
-        number /= 2;
-        tmp = remainder.ToString() + tmp;
-        
-    }
-    return tmp; //вернули результат
-
-}
-
-Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-double[] arrayFibonacci = Fibonacci(number);
-PrintArray(arrayFibonacci);
-
-double[] Fibonacci(int n)
-{
-    double[] array = new double[n];
-    array[0] = 0;
-    array[1] = 1;
-    for (int i = 2; i < array.Length; i++)
-    {
-        array[i] = array[i - 1] + array[i - 2];
-    }
-    return array;
-}
-
-void PrintArray(double[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-}
-

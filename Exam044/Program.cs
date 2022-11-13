dc63104534f5effa2f692cc695f6b5eb51d4ccfe
ElenaZateeva <zateevaelena14@gmail.com>
2022-11-13 14:03:12 +0300

    Задача Фибоначчи БЕЗ рекурсии

---
+++ Exam044/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача Фибоначчи БЕЗ рекурсии
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+double[] arrayFibonacci = Fibonacci(number);
+PrintArray(arrayFibonacci);
+
+double[] Fibonacci(int n)
+{
+    double[] array = new double[n];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+

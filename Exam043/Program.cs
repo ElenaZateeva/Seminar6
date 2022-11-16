6661c494dfc79c2c5508999c8c7ba84e39d7341c
ElenaZateeva <zateevaelena14@gmail.com>
2022-11-16 17:16:33 +0300

    Координаты точки пересечения 2х прямых

---
+++ Exam043/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+ значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+Console.WriteLine("Введите данные линейных уравнений y1=k1*+b1 и y2-k2*x+b2:k1");
+Console.WriteLine("Последовательно: k1, b1, k2, b2");
+
+bool isParsedK1 = double.TryParse(Console.ReadLine(), out double k1);
+bool isParsedB1 = double.TryParse(Console.ReadLine(), out double b1);
+bool isParsedK2 = double.TryParse(Console.ReadLine(), out double k2);
+bool isParsedB2 = double.TryParse(Console.ReadLine(), out double b2);
+if (!isParsedK1 || !isParsedK2 || !isParsedB1 || !isParsedB2)
+{
+    Console.WriteLine("Ошибка ввода данных!");
+    return;
+}
+double x=(b2 - b1)/(k1 - k2);
+double y=k1*x+b1;
+  Console.WriteLine();
+  Console.WriteLine($"Точка пересечения: ({x}; {y})");
